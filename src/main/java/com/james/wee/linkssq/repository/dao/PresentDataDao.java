af1494de25d5d8ef5436723c622476b74478264d
JamesWeeYan <JamesWeeYan@JamesWeeYan-PC>
2014-04-28 23:01:32 +0800

    add cache

--- src/main/java/com/james/wee/linkssq/repository/dao/PresentDataDao.java
+++ src/main/java/com/james/wee/linkssq/repository/dao/PresentDataDao.java
@@ -29,6 +29,7 @@ public class PresentDataDao {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(PresentDataDao.class);
+	private static final List<Presentdata> presentAllData = new ArrayList<Presentdata>();
 	private EntityManager entityManager;
 
 	public EntityManager getEntityManager() {
@@ -88,14 +89,18 @@ public class PresentDataDao {
 	 *            
 	 * @return
 	 */
-	@SuppressWarnings("unchecked")
+//	@SuppressWarnings("unchecked")
 	public List<Presentdata> queryPresentDataForPage(int pageSize,
 			int currageNo, int index) {
-		Query query = entityManager
-				.createQuery("SELECT p FROM Presentdata p ORDER BY p.id DESC");
-		query.setFirstResult((currageNo - 1) * pageSize + index);
-		query.setMaxResults(pageSize);
-		return query.getResultList();
+		if(presentAllData.isEmpty()){
+			presentAllData.addAll(queryAllPresentData());
+		}
+//		Query query = entityManager
+//				.createQuery("SELECT p FROM Presentdata p ORDER BY p.id DESC");
+//		query.setFirstResult((currageNo - 1) * pageSize + index);
+//		query.setMaxResults(pageSize);
+//		return query.getResultList();
+		return presentAllData.subList((currageNo - 1) * pageSize + index, (currageNo - 1) * pageSize + index+pageSize);
 
 	}
 
